3c5535dc2fa33936ef1fa314162897963865db24
apel-s-in <133910431+apel-s-in@users.noreply.github.com>
2026-01-23 00:07:52 +0300

    Update app-utils.js

--- scripts/app/utils/app-utils.js
+++ scripts/app/utils/app-utils.js
@@ -1,4 +1,6 @@
 // scripts/app/utils/app-utils.js
+// Тонкий фасад над window.Utils (без дублей логики).
+// Важно: не трогаем воспроизведение, только утилиты.
 
 export const $ = (id) => document.getElementById(id);
 
@@ -6,14 +8,7 @@ export const toStr = (v) => (v == null ? '' : String(v));
 
 export const isMobileUA = () => /Android|iPhone|iPad|iPod/i.test(navigator.userAgent);
 
-function _escFallback(s) {
-  const v = toStr(s);
-  return v.replace(/[<>&'"]/g, (m) => ({
-    '<': '&lt;', '>': '&gt;', '&': '&amp;', "'": '&#39;', '"': '&quot;'
-  }[m]));
-}
-
 export const escHtml = (s) => {
   const fn = window.Utils?.escapeHtml;
-  return (typeof fn === 'function') ? fn(toStr(s)) : _escFallback(s);
+  return (typeof fn === 'function') ? fn(toStr(s)) : toStr(s);
 };
