3201698dcab09285dfa97688d51cff93dae140b8
apel-s-in <133910431+apel-s-in@users.noreply.github.com>
2026-02-02 14:01:38 +0300

    Update app-utils.js

--- scripts/utils/app-utils.js
+++ scripts/utils/app-utils.js
@@ -1,19 +1,6 @@
 // scripts/app/utils/app-utils.js
-
+// Фасад над window.Utils и DOM
 export const $ = (id) => document.getElementById(id);
-
 export const toStr = (v) => (v == null ? '' : String(v));
-
-export const isMobileUA = () => /Android|iPhone|iPad|iPod/i.test(navigator.userAgent);
-
-function _escFallback(s) {
-  const v = toStr(s);
-  return v.replace(/[<>&'"]/g, (m) => ({
-    '<': '&lt;', '>': '&gt;', '&': '&amp;', "'": '&#39;', '"': '&quot;'
-  }[m]));
-}
-
-export const escHtml = (s) => {
-  const fn = window.Utils?.escapeHtml;
-  return (typeof fn === 'function') ? fn(toStr(s)) : _escFallback(s);
-};
+export const isMobileUA = () => !!window.Utils?.isMobile?.();
+export const escHtml = (s) => window.Utils?.escapeHtml ? window.Utils.escapeHtml(toStr(s)) : toStr(s);
