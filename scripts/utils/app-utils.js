deaf62f0060ec8ba7bb5b6fc95b3eab9db696804
apel-s-in <133910431+apel-s-in@users.noreply.github.com>
2026-02-04 09:44:34 +0300

    Delete scripts/utils/app-utils.js

--- scripts/utils/app-utils.js
+++
@@ -1,6 +0,0 @@
-// scripts/app/utils/app-utils.js
-// Фасад над window.Utils и DOM
-export const $ = (id) => document.getElementById(id);
-export const toStr = (v) => (v == null ? '' : String(v));
-export const isMobileUA = () => !!window.Utils?.isMobile?.();
-export const escHtml = (s) => window.Utils?.escapeHtml ? window.Utils.escapeHtml(toStr(s)) : toStr(s);
