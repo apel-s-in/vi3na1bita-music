545974b57c9e764d8f2e0985c7ab5c06871d1d4e
apel-s-in <133910431+apel-s-in@users.noreply.github.com>
2026-02-23 10:30:42 +0300

    Update stats-aggregator.js

--- scripts/analytics/stats-aggregator.js
+++ scripts/analytics/stats-aggregator.js
@@ -11,12 +11,13 @@ export class StatsAggregator {
         await metaDB.updateStat('globalFullListens', (stat) => {
           stat.value++;
           stat.details[ev.payload.uid] = (stat.details[ev.payload.uid] || 0) + 1;
-          if (stat.details[ev.payload.uid] === 5) window.dispatchEvent(new CustomEvent('analytics:cloudThresholdReached', { detail: { uid: ev.payload.uid }}));
+          if (stat.details[ev.payload.uid] >= 5) window.dispatchEvent(new CustomEvent('analytics:cloudThresholdReached', { detail: { uid: ev.payload.uid }}));
           return stat;
         });
       }
-      if (ev.type === 'LISTEN_VALID') {
-        await metaDB.updateStat('totalListenTime', (stat) => { stat.value += ev.payload.seconds; return stat; });
+      if (ev.type === 'LISTEN_VALID' || ev.type === 'LISTEN_FULL') {
+        await metaDB.updateStat('totalListenTime', (stat) => { stat.value += (ev.payload.seconds || 0); return stat; });
+        await metaDB.updateStat('lastPlayed', (stat) => { stat.details[ev.payload.uid] = ev.timestamp; return stat; });
       }
       if (ev.type === 'FEATURE_USED') {
          await metaDB.updateStat('features', (stat) => { stat.details[ev.payload.feature] = true; return stat; });
