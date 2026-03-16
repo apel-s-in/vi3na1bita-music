cb720a1c146abbe4049ffd767bc5ef733f86d9d0
apel-s-in <133910431+apel-s-in@users.noreply.github.com>
2026-03-16 22:26:42 +0300

    Create rediscovery.js

---
+++ scripts/intel/recs/rediscovery.js
@@ -0,0 +1,14 @@
+// UID.053_(Rediscovery engine)_(возвращать забытые любимые треки)_(держать отдельный источник forgotten_hits, не смешивая его с common recs)
+// UID.062_(Recommendation memory and feedback)_(не спамить одинаковыми возвратами)_(позже учитывать cooldown и recommendation_state)
+
+export const rediscovery = {
+  async init() {
+    return true;
+  },
+
+  async getCandidates() {
+    return [];
+  }
+};
+
+export default rediscovery;
