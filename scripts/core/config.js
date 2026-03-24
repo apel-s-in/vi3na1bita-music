b33efccbfe3bb4544970d927d3c874177bc79d9b
apel-s-in <133910431+apel-s-in@users.noreply.github.com>
2026-03-24 16:29:57 +0300

    Update config.js

--- scripts/core/config.js
+++ scripts/core/config.js
@@ -4,8 +4,8 @@
 // UID.092_(Incremental rollout order)_(делать staged внедрение через один конфиг)_(intel boot должен управляться отсюда)
 
 export const APP_CONFIG = {
-  APP_VERSION: '8.1.69',
-  BUILD_DATE: '2026-03-22',
+  APP_VERSION: '8.1.70',
+  BUILD_DATE: '2026-03-24',
   PROMOCODE: 'VITRINA2025',
   ICON_ALBUMS_ORDER: [
     { key: 'odnazhdy-v-skazke', title: 'Однажды в Сказке', icon: 'img/icon_album/icon-album-03.png' },
