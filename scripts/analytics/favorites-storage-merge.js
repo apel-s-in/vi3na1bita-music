1e127406b3047a9e9c02bc8439afd133fec468d4
apel-s-in <133910431+apel-s-in@users.noreply.github.com>
2026-05-05 12:34:17 +0300

    Update favorites-storage-merge.js

--- scripts/analytics/favorites-storage-merge.js
+++ scripts/analytics/favorites-storage-merge.js
@@ -1,4 +1,4 @@
-import { toNum, minPositive, getBackupConflictPolicy } from './backup-merge.js';
+import { toNum, minPositive, getBackupConflictPolicy } from './storage-merge-utils.js';
 
 const parseS = (r, f) => { try { return JSON.parse(r); } catch { return f; } };
 const favClock = i => Math.max(toNum(i?.updatedAt), toNum(i?.deletedAt), toNum(i?.inactiveAt), toNum(i?.addedAt));
