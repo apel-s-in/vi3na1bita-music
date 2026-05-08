6d19869bacea2be68760c8a39f892719cf4da3c5
apel-s-in <133910431+apel-s-in@users.noreply.github.com>
2026-05-08 18:46:56 +0300

    Update settings-data-section.js

--- scripts/app/profile/settings-data-section.js
+++ scripts/app/profile/settings-data-section.js
@@ -1,10 +1,5 @@
-import { renderConflictSettingsSection, bindConflictSettingsSection } from './settings-conflict-section.js';
-import { renderTrashSettingsSections, readDeletedPlaylists, readDeletedFavorites } from './settings-trash-section.js';
-
+import { renderConflictSettingsSection, bindConflictSettingsSection } from './settings-conflict-section.js'; import { renderTrashSettingsSections, readDeletedPlaylists, readDeletedFavorites } from './settings-trash-section.js';
 export { readDeletedPlaylists, readDeletedFavorites };
-
-export const bindDataSettingsSection = root => bindConflictSettingsSection(root);
-
+export const bindDataSettingsSection = r => bindConflictSettingsSection(r);
 export const renderDataSettingsSection = () => `<div class="settings-content" id="set-data">${renderConflictSettingsSection()}${renderTrashSettingsSections()}</div>`;
-
 export default { readDeletedPlaylists, readDeletedFavorites, renderDataSettingsSection, bindDataSettingsSection };
