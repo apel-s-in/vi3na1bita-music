4d2a62cb8092f23d549c40293bb3b33c6368c253
apel-s-in <133910431+apel-s-in@users.noreply.github.com>
2026-05-08 18:51:26 +0300

    Update yandex-modals.js

--- scripts/app/profile/yandex-modals.js
+++ scripts/app/profile/yandex-modals.js
@@ -1,19 +1,3 @@
-import { openBackupInfoModal, openBackupFoundModal } from './backup-info-modal.js';
-import { openRestorePreviewModal, openRestoreVersionPickerModal } from './restore-preview-modal.js';
-import { openFreshLoginRestoreModal } from './fresh-restore-modal.js';
-
-export {
-  openBackupInfoModal,
-  openBackupFoundModal,
-  openRestorePreviewModal,
-  openRestoreVersionPickerModal,
-  openFreshLoginRestoreModal
-};
-
-export default {
-  openBackupInfoModal,
-  openBackupFoundModal,
-  openRestorePreviewModal,
-  openRestoreVersionPickerModal,
-  openFreshLoginRestoreModal
-};
+import { openBackupInfoModal, openBackupFoundModal } from './backup-info-modal.js'; import { openRestorePreviewModal, openRestoreVersionPickerModal } from './restore-preview-modal.js'; import { openFreshLoginRestoreModal } from './fresh-restore-modal.js';
+export { openBackupInfoModal, openBackupFoundModal, openRestorePreviewModal, openRestoreVersionPickerModal, openFreshLoginRestoreModal };
+export default { openBackupInfoModal, openBackupFoundModal, openRestorePreviewModal, openRestoreVersionPickerModal, openFreshLoginRestoreModal };
