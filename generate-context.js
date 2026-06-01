c45a79d70fe72e70703e4e1bd07ae0012102687b
apel-s-in <133910431+apel-s-in@users.noreply.github.com>
2026-06-01 10:50:21 +0300

    Update generate-context.js

--- generate-context.js
+++ generate-context.js
@@ -104,6 +104,7 @@ const MUSIC_GAMES_FILES = [
   'scripts/app/games/host.js',
   'scripts/app/games/bridge-host.js',
   'scripts/app/games/config.js',
+  'scripts/app/friends/friends-block.js',
   'scripts/core/config.js',
   'scripts/utils/sw-manager.js',
   'styles/games.css',
