50c1d6b7b6273bc38180d5599457c3b394b436a8
apel-s-in <133910431+apel-s-in@users.noreply.github.com>
2026-06-12 14:09:12 +0300

    Delete scripts/app/profile/yandex-modals.js

--- scripts/app/profile/yandex-modals.js
+++
@@ -1,3 +0,0 @@
-import { openBackupInfoModal, openBackupFoundModal } from './backup-info-modal.js'; import { openRestorePreviewModal, openRestoreVersionPickerModal } from './restore-preview-modal.js'; import { openFreshLoginRestoreModal } from './fresh-restore-modal.js';
-export { openBackupInfoModal, openBackupFoundModal, openRestorePreviewModal, openRestoreVersionPickerModal, openFreshLoginRestoreModal };
-export default { openBackupInfoModal, openBackupFoundModal, openRestorePreviewModal, openRestoreVersionPickerModal, openFreshLoginRestoreModal };
